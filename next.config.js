104af2268b2ec3ee516e47d868d6b33299bb0860
Nehal Ughade <nehalughade1221@gmail.com>
2023-01-11 01:20:57 +0530

    for aws

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    loader: 'akamai',
+    path: '',
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+
 }
 
 module.exports = nextConfig
